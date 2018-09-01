29a332fe586a494a57fa781ef932694e03bb218e
divya066666 <42571843+divya066666@users.noreply.github.com>
2018-09-01 09:44:45 +0530

    Rename vowel.py to consonant.py

---
+++ consonant.py
@@ -0,0 +1,6 @@
+if 2 in("a","e","i","o","u"):
+     print("%s is a vowel:"%2)
+elif 2 =='y':
+     print("sometimes letter y stands for vowel,sometimes stands for consonants.")
+else:
+    print("%s is a consonant:"%2)
