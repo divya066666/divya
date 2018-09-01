ea82ec1aabdf74a36814f7e6e60aadbf54f3e033
divya066666 <42571843+divya066666@users.noreply.github.com>
2018-09-01 12:56:40 +0530

    Create character.py

---
+++ character.py
@@ -0,0 +1,8 @@
+ch='a'
+if ch=='0':
+   exit();
+else:
+    if((ch>='a' and ch<='z')or(ch>='A' and ch<='Z'))
+         print(ch, "is a alphabet");
+    else:
+        print(ch, "is not a alphabet");
