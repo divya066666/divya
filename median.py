91913aa6944f96d78d8a17d8cbee04860891ddc8
divya066666 <42571843+divya066666@users.noreply.github.com>
2018-09-11 11:24:45 +0530

    Create median.py

---
+++ median.py
@@ -0,0 +1,19 @@
+a = 8
+b = 7
+c = 10
+if a > b:
+    if a < c:
+        median = a
+    elif b > c:
+        median = b
+    else:
+        median = c
+else:
+    if a > c:
+        median = a
+    elif b < c:
+        median = b
+    else:
+        median = c
+
+print("The median is", median)
